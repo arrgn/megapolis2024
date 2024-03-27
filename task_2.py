33805c61d323fbad1603e4c346385789c956c5dd
Koregin IA <cemas045@vk.com>
2024-03-27 17:48:40 +0300

    Added task 2

---
+++ task_2.py
@@ -0,0 +1,16 @@
+import csv
+
+with open('history_mirror.csv', mode='r', encoding='utf-8') as pin:
+    reader = csv.reader(pin, delimiter=',')
+    fieldnames = next(reader)
+    data = list(reader)
+
+for i in range(len(data), 0, -1):
+    for j in range(1, i):
+        if data[j - 1][2] > data[j][2]:
+            temp = data[j]
+            data[j] = data[j - 1]
+            data[j - 1] = temp
+
+for row in data[:4]:
+    print(' - '.join(row))
